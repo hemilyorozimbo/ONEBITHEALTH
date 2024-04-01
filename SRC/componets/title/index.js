dddcaf1de4a3eb1fc30ee5533544a1ead65af52e
Hemily <Hemilyorozimbo771@gmail.com>
2024-04-01 11:59:34 -0300

    first commit

---
+++ SRC/componets/title/index.js
@@ -0,0 +1,12 @@
+import react from "react";
+import { View, Text } from "react-native";
+
+export default function title(){
+    return(
+        <View>
+            <Text>ONEBITHEALTH</Text>
+            <Text>01/04/2024 3b3 Lado A</Text>
+        </View>
+
+    );
+}
